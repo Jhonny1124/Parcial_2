feff194970b87b7dfaec2297309250859d1013d0
Jhonny1124 <jhonny.ortiz@udea.edu.co>
2021-09-24 18:01:11 -0500

    Promedio de colores en formato RGB para un segmento de la imagen

--- App/main.cpp
+++ App/main.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
 #include <QImage>
-#include <vector>
 
 using namespace std;
 
-#define ImageName "../Parcial_2/Imagenes/Prueba.jpg"
+#define ImageName "../Parcial_2/Imagenes/Prueba2.jpg"
 int main()
 {
     QImage imagen(ImageName);
 
-
-    unsigned short int ancho = imagen.height(), alto = imagen.width();
+    unsigned short int ancho = imagen.width(), alto = imagen.height();
     unsigned short int Rojo[alto][ancho], Verde[alto][ancho], Azul[alto][ancho];
-
+    int SumaRojo = 0, SumaVerde = 0, SumaAzul = 0;
     for(unsigned short int i = 0; i < alto; i++){
         for(unsigned short int l = 0; l < ancho; l++){
-            Rojo[i][l] = imagen.pixelColor(i, l).red();
-            Verde[i][l]= imagen.pixelColor(i, l).green();
-            Azul[i][l] = imagen.pixelColor(i, l).blue();
+            Rojo[i][l] = imagen.pixelColor(l, i).red();
+            Verde[i][l]= imagen.pixelColor(l, i).green();
+            Azul[i][l] = imagen.pixelColor(l, i).blue();
         }
     }
 
+     for(int i = 0; i < alto/12; i++){
+        for(int l = 0; l < ancho/12; l++){
+            SumaRojo += imagen.pixelColor(l, i).red();
+            SumaVerde += imagen.pixelColor(l, i).green();
+            SumaAzul += imagen.pixelColor(l, i).blue();
+        }
+    }
+    SumaRojo /= ((ancho/12)*(alto/12));
+    SumaVerde /= ((ancho/12)*(alto/12));
+    SumaAzul /= ((ancho/12)*(alto/12));
+     cout << SumaRojo << "," << SumaVerde << "," << SumaAzul <<endl;
     return 0;
 }
