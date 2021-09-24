59600d9d11d3b46606d0cce9c329ffe7c57ddba0
Jhonny1124 <jhonny.ortiz@udea.edu.co>
2021-09-23 19:47:08 -0500

    Abrir una imagen en Qt

---
+++ App/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <QImage>
+
+using namespace std;
+
+#define ImageName "../Parcial_2/Imagenes/Prueba.jpg"
+int main()
+{
+    QImage imagen(ImageName);
+
+    return 0;
+}
