0575a7a18441c15bb26527fe26d969f1352a53b8
Jhonny1124 <jhonny.ortiz@udea.edu.co>
2021-09-23 21:32:01 -0500

    Saque la informacion de los colores en formato RGB de la imagen

--- App/main.cpp
+++ App/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <QImage>
+#include <vector>
 
 using namespace std;
 
@@ -7,6 +8,18 @@ using namespace std;
 int main()
 {
     QImage imagen(ImageName);
+
+
+    unsigned short int ancho = imagen.height(), alto = imagen.width();
+    unsigned short int Rojo[alto][ancho], Verde[alto][ancho], Azul[alto][ancho];
+
+    for(unsigned short int i = 0; i < alto; i++){
+        for(unsigned short int l = 0; l < ancho; l++){
+            Rojo[i][l] = imagen.pixelColor(i, l).red();
+            Verde[i][l]= imagen.pixelColor(i, l).green();
+            Azul[i][l] = imagen.pixelColor(i, l).blue();
+        }
+    }
 
     return 0;
 }
