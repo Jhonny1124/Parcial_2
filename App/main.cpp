9ecf0c34b53f5486a4a510467a0cd99ee5c008e3
Jhonny1124 <jhonny.ortiz@udea.edu.co>
2021-09-24 20:17:47 -0500

    Avance en el submuestreo de imagenes

--- App/main.cpp
+++ App/main.cpp
@@ -10,7 +10,7 @@ int main()
 
     unsigned short int ancho = imagen.width(), alto = imagen.height();
     unsigned short int Rojo[alto][ancho], Verde[alto][ancho], Azul[alto][ancho];
-    int SumaRojo = 0, SumaVerde = 0, SumaAzul = 0;
+    int SumaRojo = 0, SumaVerde = 0, SumaAzul = 0, cont1 = -1, cont2 = -1;
     for(unsigned short int i = 0; i < alto; i++){
         for(unsigned short int l = 0; l < ancho; l++){
             Rojo[i][l] = imagen.pixelColor(l, i).red();
@@ -18,17 +18,24 @@ int main()
             Azul[i][l] = imagen.pixelColor(l, i).blue();
         }
     }
-
-     for(int i = 0; i < alto/12; i++){
-        for(int l = 0; l < ancho/12; l++){
-            SumaRojo += imagen.pixelColor(l, i).red();
-            SumaVerde += imagen.pixelColor(l, i).green();
-            SumaAzul += imagen.pixelColor(l, i).blue();
+    while(cont1 < 11){
+        ++cont1;
+        cont2 = -1;
+        while(cont2 < 11){
+            SumaRojo = 0, SumaVerde = 0, SumaAzul = 0;
+            ++cont2;
+            for(int i = (alto/12)*cont1; i < (alto/12)*(cont1+1); i++){
+               for(int l = (ancho/12)*cont2; l < (ancho/12)*(cont2+1); l++){
+                   SumaRojo += imagen.pixelColor(l, i).red();
+                   SumaVerde += imagen.pixelColor(l, i).green();
+                   SumaAzul += imagen.pixelColor(l, i).blue();
+               }
+           }
+           SumaRojo /= ((ancho/12)*(alto/12));
+           SumaVerde /= ((ancho/12)*(alto/12));
+           SumaAzul /= ((ancho/12)*(alto/12));
         }
     }
-    SumaRojo /= ((ancho/12)*(alto/12));
-    SumaVerde /= ((ancho/12)*(alto/12));
-    SumaAzul /= ((ancho/12)*(alto/12));
-     cout << SumaRojo << "," << SumaVerde << "," << SumaAzul <<endl;
+
     return 0;
 }
